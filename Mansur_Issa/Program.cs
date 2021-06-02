2434f0b7d59a3c05892111447e87f590b5292ea6
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 12:52:54 +0300

    Add files via upload

---
+++ Mansur_Issa/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j=0; j < 4; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                    Console.Write(" ");
+                    Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
