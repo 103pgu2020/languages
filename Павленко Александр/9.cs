a9d6f33b5ace97f7f7c1075adaaafc0dd4b3b343
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 17:12:33 +0300

    Create 9.cs

---
+++ Павленко Александр/9.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m;
+        m1: Console.WriteLine("Программа, выводящая шахматную доску");
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Если хотите повторить программу введите '1'");
+            m = Convert.ToInt32(Console.ReadLine());
+            if (m == 1)
+            {
+                goto m1;
+            }
+            Console.ReadLine();
+            return;
+        }
+    }
+}
