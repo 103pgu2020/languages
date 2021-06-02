d7c9456c96662135afd49e08881bf5a274453ad7
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 20:11:51 +0300

    Add files via upload

---
+++ Fediunin/Program3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int count = 0;
+			Console.WriteLine("Программа для подсчёта количества смен знака");
+		m: Console.WriteLine("Вв-те количество чисел последовательности");
+			int n = Convert.ToInt32(Console.ReadLine());
+			if (n < 0)
+			{
+				Console.WriteLine("Число не целое или меньше нуля. Повторите попытку.");
+				goto m;
+			}
+			int[] arr = new int[n];
+			for (int i = 0; i < n; i++)
+			{
+				Console.WriteLine("Вв-те {0} член последовательности", i + 1);
+				arr[i] = Int32.Parse(Console.ReadLine());
+			}
+			for (int i = 0; i < n - 1; i++)
+			{
+				if ((arr[i] < 0 && arr[i + 1] >= 0) || (arr[i] >= 0 && arr[i + 1] < 0))
+					count++;
+			}
+			Console.WriteLine("Количество смен знака(ов) " + count);
+			Console.WriteLine("Повторить? 1-Да 0-нет.");
+			int a = Convert.ToInt32(Console.ReadLine());
+			if (a == 1)
+				goto m;
+			return;
+
+		}
+	}
+}
