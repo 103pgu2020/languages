612d199bb2f8c9cd9b8d47814a72e871d513090c
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 22:14:28 +0300

    Add files via upload

---
+++ Куликов Евгений/Program18.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        m1: Console.Write("N: ");
+            int N = int.Parse(Console.ReadLine());
+            if (N < 1)
+            {
+                Console.WriteLine("Введённое число не подходит по условию");
+                goto m1;
+            }
+            int c = 2;
+            for (int i = 1; i <= N / 10; i++)
+            {
+                if (i % 3 != 0)
+                    c *= 2;
+            }
+            Console.WriteLine(c);
+            Console.Write("\nЕщё раз? (1 - да, 0 - нет) ");
+            if (Console.ReadLine() == "1")
+            {
+                Console.Clear();
+                goto m1;
+            }
+        }
+    }
+}
