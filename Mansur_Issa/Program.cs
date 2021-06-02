e889b9a459b8cf319eaba6fd258e0bce78e5285a
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 12:54:44 +0300

    Add files via upload

---
+++ Mansur_Issa/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        st:
+            Console.Clear();
+
+            int N = 0;
+            while (N < 1)
+            {
+                Console.WriteLine("Введите натуральное число:");
+                N = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Делители:");
+            int k = 0;
+            for (int i = 1; i <= N / 2; i++)
+            {
+                if (N % i == 0)
+                {
+                    Console.Write(i + " ");
+                    k++;
+                }
+            }
+            Console.Write(N);
+            k++;
+            Console.WriteLine("\nКоличество делителей: " + k);
+            
+            Console.WriteLine("\nPress 1 for restart, any other - exit.");
+            int x = Int32.Parse(Console.ReadLine());
+            if (x == 1)
+                goto st;
+        }
+    }
+}
