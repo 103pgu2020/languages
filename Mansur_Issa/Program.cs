1db4e8d0de49d722a3da08c773338b668061d8aa
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 12:47:06 +0300

    Add files via upload

---
+++ Mansur_Issa/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        st:
+            Console.Clear();
+            Console.WriteLine("Вводите последовательность:");
+            int k = 0;
+            double i, j = 0;
+            while ((i = Double.Parse(Console.ReadLine()))!=0)
+            {
+                if (i * j < 0)
+                    k++;
+                j = i;
+            }
+            Console.WriteLine("Количество смен знаков: " + k);
+            Console.WriteLine("\nPress 1 for restart, any other - exit.");
+            k = Int32.Parse(Console.ReadLine());
+            if (k == 1)
+                goto st;
+        }
+    }
+}
