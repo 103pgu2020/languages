64cd73115883bc2342c81a773c7e70939c06e42e
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 19:22:51 +0300

    Add files via upload

--- Карпенко Владимир/1.cs
+++ Карпенко Владимир/1.cs
@@ -1,1 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа для вычисления количество цифр в данном числе");
+        m: Console.Write("Введите число: ");
+            int  count = 0;
+            int x = Int32.Parse(Console.ReadLine());
+            if (x == 0)
+                count++;
+            while (x != 0)
+            {
+                count++;
+                x /= 10;
+            }
+            Console.WriteLine("Количество цифр: " + count);
+            Console.WriteLine("Еще раз? Да - 1, нет - 0");
+            int r = Int32.Parse(Console.ReadLine());
+            if (r == 1) goto m;
+            return;
+        }
+    }
+}
