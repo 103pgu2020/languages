f91f205153bc403ad3ff171570d0c2e962011f7c
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 12:44:45 +0300

    Add files via upload

---
+++ Mansur_Issa/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        st:
+            Console.Clear();
+            Console.WriteLine("Программа для нахождения количества цифр в числе.");
+            Console.WriteLine("Введите число:");
+            int x = Int32.Parse(Console.ReadLine()), i = 0;
+            if (x == 0)
+                i++;
+            while (x!=0)
+            {
+                i++;
+                x /= 10;
+            }
+            Console.WriteLine("Количество цифр в числе = " + i);
+            Console.WriteLine("\nPress 1 for restart, any other - exit.");
+            x = Int32.Parse(Console.ReadLine());
+            if (x == 1)
+                goto st;
+        }
+    }
+}
