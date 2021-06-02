21ef24250bf3f371d8a25c534ede68d8dfa3c169
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 12:50:39 +0300

    Add files via upload

---
+++ Mansur_Issa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        st:
+            Console.Clear();
+            Console.WriteLine("Подходящие числа:");
+            for (int i = 100; i <= 999; i++)
+            {
+                int s = i / 100;
+                int d = i / 10 % 10;
+                int e = i % 10;
+                if ((s==d || d==e || s==e) && i % 111 != 0)
+                    Console.Write(i + " ");
+            }
+            Console.WriteLine("\nPress 1 for restart, any other - exit.");
+            int x = Int32.Parse(Console.ReadLine());
+            if (x == 1)
+                goto st;
+        }
+    }
+}
