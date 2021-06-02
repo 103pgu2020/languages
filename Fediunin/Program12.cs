526373c11df6bed0ebf27dc3060aa377539fc1ee
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 20:57:07 +0300

    Add files via upload

---
+++ Fediunin/Program12.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int N, k = 0, i = 0;
+			Console.WriteLine("Программа нахождения делителей");
+		m1: Console.WriteLine("Введите натуральное число N");
+			Console.Write("N: ");
+			N = Convert.ToInt16(Console.ReadLine());
+			if (N < 1)
+			{
+				Console.WriteLine("Ошибка ввода повторите ещё");
+				goto m1;
+			}
+			Console.WriteLine("Делители:");
+			for (i = 1; i <= N; i++)
+			{
+				if (N % i == 0)
+				{
+					Console.WriteLine(i);
+					k++;
+				}
+			}
+			Console.WriteLine("Кол-во делителей:" + k);
+			Console.WriteLine("Продолжить? (1 - да 0 - нет) ");
+			if (Console.ReadLine() == "1")
+			{
+				Console.Clear();
+				goto m1;
+			}
+			else
+			return;
+		}
+	}
+}
