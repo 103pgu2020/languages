2d46c93bf2b6683710ac35fee22051c518f164c4
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 10:39:35 +0300

    Add files via upload

---
+++ Макалич Денис/10.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 1; j <= 9; j++)
+                Console.Write("{0,4}", i * j);
+            Console.WriteLine("\n");
+        }
+        Console.ReadKey();
+    }
+}
