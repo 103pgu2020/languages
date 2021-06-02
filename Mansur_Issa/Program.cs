a70cb941658b39d60f472b6f820f06c8f71b7bf8
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 12:53:35 +0300

    Add files via upload

---
+++ Mansur_Issa/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //st:
+            Console.Clear();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 0; j <= 10; j++)
+                    if (i+j == 0)
+                        Console.Write("    ");
+                    else if (i*j == 0)
+                        Console.Write("{0,4}", i+j);
+                    else
+                        Console.Write("{0,4}", i*j);
+                Console.WriteLine('\n');
+            }
+
+            Console.SetCursorPosition(0, 1);
+            Console.Write(new String('-', 44));
+            for (int i = 0; i < 21; i++)
+            {
+                Console.SetCursorPosition(5, i);
+                Console.Write("|");
+            }
+            Console.SetCursorPosition(5, 1);
+            Console.Write("+");
+            Console.SetCursorPosition(0, 21);
+            /*
+            Console.WriteLine("\nPress 1 for restart, any other - exit.");
+            int x = Int32.Parse(Console.ReadLine());
+            if (x == 1)
+                goto st;
+            */
+        }
+    }
+}
