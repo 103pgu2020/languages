fe188d665349bc1b199b9232300d545b8b73fd95
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 12:55:32 +0300

    Add files via upload

---
+++ Mansur_Issa/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        st:
+            Console.Clear();
+
+            int N = 0;
+            while (N < 1)
+            {
+                Console.WriteLine("Введите натуральное число:");
+                N = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Простые числа меньше заданного:");
+            int k = 0;
+            for (int i = 2; i < N; i++)
+            {
+                if ((i < 4 || (i+1)%6 == 0 || (i-1)%6 == 0) && (i == 5 || i%5!=0) && (i == 7 || i % 7 != 0))
+                {
+                    Console.Write(i + " ");
+                    k++;
+                }
+            }
+            if (k == 0)
+                Console.WriteLine("Простые числа не найдены!");
+
+            Console.WriteLine("\nPress 1 for restart, any other - exit.");
+            int x = Int32.Parse(Console.ReadLine());
+            if (x == 1)
+                goto st;
+        }
+    }
+}
