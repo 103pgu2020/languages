30d0ee95c870022e1a0da0e7f1b1394871d73791
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 20:55:03 +0300

    Add files via upload

---
+++ Fediunin/Program9.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		m: Console.WriteLine("Программа, которая выводи на экран схему шахматной доски");
+			for (int i = 0; i < 8; i++)
+			{
+				for (int k = 0; k < 8; k++)
+				{
+					if ((i + k) % 2 == 0)
+						Console.Write((char)'\u2588');
+					else
+						Console.Write(" ");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine("Повторить? 1-Да 0-нет.");
+			int l = Convert.ToInt32(Console.ReadLine());
+			if (l == 1)
+				goto m;
+			return;
+		}
+	}
+}
