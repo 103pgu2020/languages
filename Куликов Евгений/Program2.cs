17c7a649608bb10ac8a66b815632e186cd6cb619
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 22:08:06 +0300

    Add files via upload

---
+++ Куликов Евгений/Program2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        m1: Console.Write("Стартовый капитал: ");
+            int k = int.Parse(Console.ReadLine());
+            Console.Write("Ежемесячное процентное увеличение: ");
+            int p = int.Parse(Console.ReadLine());
+            Console.Write("Сумма для накопления: ");
+            int S = int.Parse(Console.ReadLine());
+            int m = 0;
+            if (k >= 0 && p > 0 && S >= k)
+            {
+                while (k <= S)
+                {
+                    k += k / 100 * p;
+                    m++;
+                }
+                Console.WriteLine($"Сумма накопится через {m} месяцев");
+            }
+            else Console.WriteLine("Введите число согласно условию задачи");
+            Console.Write("Ещё раз? (1 - да, 0 - нет) ");
+            if (Console.ReadLine() == "1")
+            {
+                Console.Clear();
+                goto m1;
+            }
+        }
+    }
+}
