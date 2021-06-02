bc77f8cf465494af5fccb683970c47f2ead5b149
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 20:55:20 +0300

    Add files via upload

---
+++ Fediunin/Program10.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10
+{
+	class Program
+	{
+		static void Main()
+		{
+			for (int i = 1; i <= 9; i++)
+			{
+				for (int j = 1; j <= 9; j++)
+					Console.Write("{0,4}", i * j);
+				Console.WriteLine("\n");
+			}
+			Console.ReadKey();
+		}
+	}
+}
