58e94b1db9ed6b3b3da1c5cedfb81d23ef54b891
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 22:09:30 +0300

    Add files via upload

---
+++ Куликов Евгений/Program6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        m1: Console.Write("N: ");
+            int N = int.Parse(Console.ReadLine());
+            int a, b, c;
+            for (int i = 100; i < 1000; i++)
+            {
+                a = i / 100;
+                b = i / 10 % 10;
+                c = i % 10;
+
+                if ((a + b + c) < N)
+                    Console.WriteLine(i);
+            }
+            Console.Write("Ещё раз? (1 - да, 0 - нет) ");
+            if (Console.ReadLine() == "1")
+            {
+                Console.Clear();
+                goto m1;
+            }
+        }
+    }
+}
