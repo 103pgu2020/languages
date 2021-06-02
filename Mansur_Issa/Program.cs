629c244a184be0090101ad316f7c1f749b5029a9
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 12:57:17 +0300

    Add files via upload

---
+++ Mansur_Issa/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        st:
+            Console.Clear();
+
+            Random random = new Random();
+            int N = random.Next(1000) + 1;
+
+            Console.WriteLine("Привет, Игрок! Твоя цель угадать число от 1 до 1000 за 10 попыток.");
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Попытка №"+i);
+                Console.WriteLine("Введите число:");
+                int y = Int32.Parse(Console.ReadLine());
+                if (y == N)
+                {
+                    Console.WriteLine("Угадал(а)");
+                    break;
+                }
+                else
+                    Console.WriteLine("Загаданное число " + (N > y ? "больше":"меньше"));
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nPress 1 for restart, any other - exit.");
+            int x = Int32.Parse(Console.ReadLine());
+            if (x == 1)
+                goto st;
+        }
+    }
+}
