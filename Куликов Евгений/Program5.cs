c133959716c7fd3bbf3a627edc0a72c69a1ba606
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 22:09:16 +0300

    Add files via upload

---
+++ Куликов Евгений/Program5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd5
+{
+    class Program
+    {
+        static Random rand = new Random();
+        static void Output()
+        {
+            double mean = 0;
+            Console.Write("Последовательность (из 10 чисел): ");
+            for (int i = 0; i < 10; i++)
+            {
+                int num = rand.Next(-100, 100);
+                Console.Write($"{num} ");
+                mean += num;
+            }
+            mean /= 10;
+            Console.WriteLine($"\nСреднее арифметическое: {mean}\n");
+        }
+        static void Main(string[] args)
+        {
+        m1:
+            for (int i = 0; i < 3; i++)
+                Output();
+            Console.Write("Повторить? (1 - да, 0 - нет) ");
+            if (Console.ReadLine() == "1")
+            {
+                Console.Clear();
+                goto m1;
+            }
+        }
+    }
+}
