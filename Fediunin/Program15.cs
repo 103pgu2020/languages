f5f5e78f48573ce203ea6a47ddeb88365d39d9c6
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 21:01:24 +0300

    Add files via upload

---
+++ Fediunin/Program15.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Вывести на экран первые k членов ряда Фибоначчи. (1, 1, 2, 3, 5, 8...)");
+		m1: Console.Write("Сколько нужно вывести членов последовательности Фибоначчи? N: ");
+			int n, a = 0, b = 1, s = 0, k = 1;
+			n = Int32.Parse(Console.ReadLine());
+			if (n < 0) { Console.WriteLine("Ошибка! Введите другое значение!"); goto m1; }
+			while (k <= n)
+			{
+				s = a + b;
+				a = b;
+				b = s;
+				k++;
+				Console.Write(a + " ");
+			}
+			Console.WriteLine();
+			Console.WriteLine("Еще раз? (1 - да 0- нет");
+			int u = Int32.Parse(Console.ReadLine());
+			if (u == 1) goto m1;
+			return;
+		}
+	}
+}
