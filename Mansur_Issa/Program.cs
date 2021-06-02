5dbf611b738813917d7c5e486c46e782042f072b
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 12:56:43 +0300

    Add files via upload

---
+++ Mansur_Issa/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        st:
+            Console.Clear();
+
+            int k = 0;
+            int f = 1, s = 0;
+            while (k < 1)
+            {
+                Console.WriteLine("Введите натуральное число:");
+                k = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Числа Фибоначчи ({0} штук):", k);
+            for (int i=1; i<=k; i++)
+            {
+                f = (s+=f) - f;
+                Console.Write(f + " ");
+            }
+
+            Console.WriteLine("\nPress 1 for restart, any other - exit.");
+            int x = Int32.Parse(Console.ReadLine());
+            if (x == 1)
+                goto st;
+        }
+    }
+}
