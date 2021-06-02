836ba4fb0c2e7f375190da0cd4bf5ff98c53f9dd
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 22:13:26 +0300

    Add files via upload

---
+++ Program16.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+        m1: int n = rand.Next(1, 1001);
+            Console.WriteLine("Загадано число от 1 до 1000");
+            int a;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"{i} попытка: ");
+                a = int.Parse(Console.ReadLine());
+
+                if (a > n)
+                    Console.WriteLine("Загаданное число меньше\n");
+                else if (a < n)
+                    Console.WriteLine("Загаданное число больше\n");
+                else
+                {
+                    Console.WriteLine("Угадали");
+                    break;
+                }
+            }
+            Console.Write("\nЕщё раз? (1 - да, 0 - нет) ");
+            if (Console.ReadLine() == "1")
+            {
+                Console.Clear();
+                goto m1;
+            }
+        }
+    }
+}
