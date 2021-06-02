ee61a84d6a755b981dd3b6cce065686c1861cbf3
103pgu2020 <85182562+103pgu2020@users.noreply.github.com>
2021-06-02 12:51:52 +0300

    Add files via upload

---
+++ Mansur_Issa/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        st:
+            Console.Clear();
+            Console.WriteLine("Введите N:");
+            int N = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Искомые цифры:");
+            for (int i = 9; i >= 0; i--)
+            {
+                int n = N;
+                int p = 10;
+                while (n != 0 && p != i)
+                {
+                    p = n % 10;
+                    n = n / 10;
+                }
+                if (p != i)
+                    Console.Write(i + " ");
+            }
+            Console.WriteLine("\nPress 1 for restart, any other - exit.");
+            int x = Int32.Parse(Console.ReadLine());
+            if (x == 1)
+                goto st;
+        }
+    }
+}
